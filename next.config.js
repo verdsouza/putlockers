ef01fb69e8b5bbb329a7dfe414e1cd179f2b36f3
verdsouza <dveronica434@gmail.com>
2026-02-13 21:44:08 +0530

    pk fuck it now

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
+  swcMinify: true,
+    images: {
     remotePatterns: [
       {
         protocol: 'https',
